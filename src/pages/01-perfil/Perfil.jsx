f372b74acfa353cd27bdad2389b8ff5a0d187567
Matias Montenegro <matiasmontenegro0810@gmail.com>
2025-04-25 18:13:52 -0300

    maquetado simple componente perfil

--- src/pages/01-perfil/Perfil.jsx
+++ src/pages/01-perfil/Perfil.jsx
@@ -3,8 +3,14 @@ import './perfil.css'
 
 const Perfil = () => {
     return (
-        <section className="perfil">
-            <h1>Componente Perfil</h1>
+        <section className="perfil-container">
+            <div className='foto-perfil'><img src="#" alt="" /></div>
+            <h1>Montenegro Matias</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero assumenda ipsum necessitatibus laborum blanditiis, dolor quae doloribus iste tempora dolorum.</p>
+            <div className="btn-container">
+                <div className="redes">Iconos redes</div>
+                <div className="btn-cv"><p>Descargar CV</p></div>
+            </div>
         </section>
     )
 }
