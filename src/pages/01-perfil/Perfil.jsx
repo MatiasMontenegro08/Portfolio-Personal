57a34c1a0b7006fea0f82cbc1b35555b5f4bc9f3
Matias Montenegro <matiasmontenegro0810@gmail.com>
2025-08-25 16:07:02 -0300

    actualizacion de CV

--- src/pages/01-perfil/Perfil.jsx
+++ src/pages/01-perfil/Perfil.jsx
@@ -9,7 +9,7 @@ const Perfil = () => {
                 img={imgPerfil}
                 nombre={"Montenegro Matias"}
                 descripcion={"Soy una persona proactiva, organizada y responsable, con capacidad para trabajar en equipo. Me gusta aprender, asumir nuevos desafíos y busco incorporarme a un equipo donde pueda aportar mis habilidades y seguir creciendo profesionalmente."}
-                linkcv={"/CV_matias-nicolas-montenegro.pdf"}
+                linkcv={"/CV-MontenegroMatias.pdf"}
             />
         </section>
     )
