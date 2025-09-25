bda9b76daf68d82e506a9d6ad0aae2effad2a77d
Matias Montenegro <matiasmontenegro0810@gmail.com>
2025-09-25 18:32:44 -0300

    Nuevo proyecto, ajuste imagen-link de la card

--- src/components/card-proyecto/Cardproyecto.jsx
+++ src/components/card-proyecto/Cardproyecto.jsx
@@ -4,7 +4,7 @@ import './cardproyecto.css'
 const Cardproyecto = ({img, titulo, descripcion, children, linkVer, linkRepo}) => {
     return (
         <div className="tarjeta-proyecto">
-            <img src={img} alt="Logo Proyecto" />
+            <Link to={linkVer} target='_blank' rel="noopener noreferrer"><img src={img} alt="Logo Proyecto" /></Link>
             <h2>{titulo}</h2>
             <p>{descripcion}</p>
             <div className='container-icons'>
@@ -12,10 +12,10 @@ const Cardproyecto = ({img, titulo, descripcion, children, linkVer, linkRepo}) =
             </div>
             <div className="container-links">
                 <div className='links-proyecto'>
-                    <Link to={linkVer}><FaEye className='icon-links' /></Link>
+                    <Link to={linkVer} target='_blank' rel="noopener noreferrer"><FaEye className='icon-links' /></Link>
                 </div>
                 <div className='links-proyecto'>
-                    <Link to={linkRepo}><FaGithub className='icon-links' /></Link>
+                    <Link to={linkRepo} target='_blank' rel="noopener noreferrer"><FaGithub className='icon-links' /></Link>
                 </div>
             </div>
         </div>
--- src/pages/02-proyectos/Proyectos.jsx
+++ src/pages/02-proyectos/Proyectos.jsx
@@ -1,5 +1,9 @@
-import imgProyecto from '../../assets/sinimagen.jpg'
+import imgProyecto from '../../assets/catalogopegaya.svg'
 import { FaCss3, FaHtml5, FaJs } from 'react-icons/fa'
+import { GrReactjs } from 'react-icons/gr'
+import { TbBrandVite } from "react-icons/tb";
+import { SiFirebase } from "react-icons/si";
+import { RiBootstrapLine } from "react-icons/ri";
 import Cardproyecto from '../../components/card-proyecto/Cardproyecto'
 import './proyectos.css'
 
@@ -9,16 +13,21 @@ const Proyectos = () => {
             <h1>Proyectos</h1>
             <Cardproyecto
                 img={imgProyecto}
-                titulo="Proyecto 1"
-                descripcion="Descripción del proyecto 1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus!"
-                linkVer="#"
-                linkRepo="#"
+                titulo="Catálogo Pega-Ya"
+                descripcion="Catálogo de stickers personalizado para un emprendimiento local. Permite a los usuarios explorar y seleccionar entre una variedad de stickers, agregarlos a su carrito, con la opción de poder copiar el pedido y enviarlo por Instagram."
+                linkVer="https://catalogo-pega-ya.vercel.app/"
+                linkRepo="https://github.com/MatiasMontenegro08/Catalogo-PegaYa.git"
             >
+                <div className='tec-container'><GrReactjs className='tec-icon'/><p>Ract</p></div>
                 <div className='tec-container'><FaHtml5 className='tec-icon'/><p>HTML</p></div>
                 <div className='tec-container'><FaCss3 className='tec-icon'/><p>CSS</p></div>
                 <div className='tec-container'><FaJs className='tec-icon'/><p>JavaScript</p></div>
+                <div className='tec-container'><TbBrandVite className='tec-icon'/><p>Vite</p></div>
+                <div className='tec-container'><SiFirebase className='tec-icon'/><p>Firebase</p></div>
+                <div className='tec-container'><RiBootstrapLine className='tec-icon'/><p>Bootstrap</p></div>
+
             </Cardproyecto>
-            <Cardproyecto
+            {/* <Cardproyecto
                 img={imgProyecto}
                 titulo="Proyecto 2"
                 descripcion="Descripción del proyecto 2 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus!"
@@ -28,7 +37,7 @@ const Proyectos = () => {
                 <div className='tec-container'><FaHtml5 className='tec-icon'/><p>HTML</p></div>
                 <div className='tec-container'><FaCss3 className='tec-icon'/><p>CSS</p></div>
                 <div className='tec-container'><FaJs className='tec-icon'/><p>JavaScript</p></div>
-            </Cardproyecto>
+            </Cardproyecto> */}
         </section>
     )
 }
